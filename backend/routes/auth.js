ecb5e8e24945e0c4dacf87809e07cd24e0be8eff
Ishita <vatsishita145@gmail.com>
2024-12-28 15:33:00 +0530

    Check update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,5 +3,8 @@ import { login } from '../controllers/authControllers.js';
 const router = express.Router();
 
 router.post('/login', login);
+router.get("", (req, res) => {
+    res.send("Hello from the backend");
+})
 
 export default router;
